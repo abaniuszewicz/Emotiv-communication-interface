bfa0cfd49fb16df84f8a4cae7b8f348381716a0d
abaniuszewicz <abaniuszewicz@gmail.com>
2019-08-30 02:44:16 +0200

    Changed tests for parameters, updated accordingly chap 4.2, added notes on integration with Insight

--- praca/badania/badania2_plot.m
+++ praca/badania/badania2_plot.m
@@ -2,20 +2,20 @@ threshold = 0.1:0.1:1;
 
 frown_s_base = 100;
 frown_ns_base = 50;
-frown_s_500 = [50 70 90 100 90 100 80 80 70 70];
+frown_s_500 = [40 50 70 80 80 90 100 90 60 20];
 frown_ns_500 = [30 20 20 10 0 0 0 0 0 0];
-frown_s_1500 = [50 80 80 90 100 100 50 20 10 10];
+frown_s_1500 = [100 100 100 100 80 80 70 60 20 0];
 frown_ns_1500 = [20 20 0 0 0 0 0 0 0 0];
-frown_s_2500 = [50 30 10 0 0 0 0 0 0 0];
+frown_s_2500 = [90 60 60 30 0 0 0 0 0 0];
 frown_ns_2500 = [0 0 0 0 0 0 0 0 0 0];
 
 up_s_base = 90;
 up_ns_base = 50;
-up_s_500 = [100 100 100 100 90 80 60 40 0 0];
-up_ns_500 = [100 100 100 90 90 90 70 30 0 0];
-up_s_1500 = [100 100 80 60 10 0 0 0 0 0];
+up_s_500 = [0 0 0 10 10 20 20 10 0 0];
+up_ns_500 = [100 100 100 90 90 70 60 20 0 0];
+up_s_1500 = [0 0 20 40 10 0 0 0 0 0];
 up_ns_1500 = [100 100 70 50 0 0 0 0 0 0];
-up_s_2500 = [100 60 50 50 10 0 0 0 0 0];
+up_s_2500 = [0 30 40 40 10 0 0 0 0 0];
 up_ns_2500 = [100 60 60 40 0 0 0 0 0 0];
 
 
@@ -26,9 +26,9 @@ xlim([0.1 1]); ylim([0 100])
 xlabel('Threshold'); ylabel('Stosunek detekcji do iloœci prób [%]')
 plot(threshold, frown_s_500, '-o');
 plot(threshold, frown_ns_500, '--s');
-plot(threshold, frown_s_base * ones(size(threshold)), '-', 'LineWidth', 2.0);
-set(gca,'ColorOrderIndex',3)
-plot(threshold, frown_ns_base * ones(size(threshold)), '--', 'LineWidth', 2.0);
+% plot(threshold, frown_s_base * ones(size(threshold)), '-', 'LineWidth', 2.0);
+% set(gca,'ColorOrderIndex',3)
+% plot(threshold, frown_ns_base * ones(size(threshold)), '--', 'LineWidth', 2.0);
 legend('Detekcja podczas wykonania','Detekcja podczas braku wykonania', 'Location','best');
 
 frown1500 = figure;
@@ -37,10 +37,10 @@ xlim([0.1 1]); ylim([0 100])
 xlabel('Threshold'); ylabel('Stosunek detekcji do iloœci prób [%]')
 plot(threshold, frown_s_1500, '-o');
 plot(threshold, frown_ns_1500, '--s');
-plot(threshold, frown_s_base * ones(size(threshold)), '-', 'LineWidth', 2.0);
-set(gca,'ColorOrderIndex',3)
-plot(threshold, frown_ns_base * ones(size(threshold)), '--', 'LineWidth', 2.0);
-legend('Detekcja podczas wykonania','Detekcja podczas braku wykonania', 'Location','best');
+% plot(threshold, frown_s_base * ones(size(threshold)), '-', 'LineWidth', 2.0);
+% set(gca,'ColorOrderIndex',3)
+% plot(threshold, frown_ns_base * ones(size(threshold)), '--', 'LineWidth', 2.0);
+legend('Detekcja podczas wykonania','Detekcja podczas braku wykonania', 'Location','northeast');
 
 frown2500 = figure;
 hold on
@@ -48,10 +48,10 @@ xlim([0.1 1]); ylim([0 100])
 xlabel('Threshold'); ylabel('Stosunek detekcji do iloœci prób [%]')
 plot(threshold, frown_s_2500, '-o');
 plot(threshold, frown_ns_2500, '--s');
-plot(threshold, frown_s_base * ones(size(threshold)), '-', 'LineWidth', 2.0);
-set(gca,'ColorOrderIndex',3)
-plot(threshold, frown_ns_base * ones(size(threshold)), '--', 'LineWidth', 2.0);
-legend('Detekcja podczas wykonania','Detekcja podczas braku wykonania', 'Location','best');
+% plot(threshold, frown_s_base * ones(size(threshold)), '-', 'LineWidth', 2.0);
+% set(gca,'ColorOrderIndex',3)
+% plot(threshold, frown_ns_base * ones(size(threshold)), '--', 'LineWidth', 2.0);
+legend('Detekcja podczas wykonania','Detekcja podczas braku wykonania', 'Location','northeast');
 
 set(frown500, 'Units', 'Inches');
 pos = get(frown500, 'Position');
@@ -76,10 +76,10 @@ xlim([0.1 1]); ylim([0 100])
 xlabel('Threshold'); ylabel('Stosunek detekcji do iloœci prób [%]')
 plot(threshold, up_s_500, '-o');
 plot(threshold, up_ns_500, '--s');
-plot(threshold, up_s_base * ones(size(threshold)), '-', 'LineWidth', 2.0);
-set(gca,'ColorOrderIndex',3)
-plot(threshold, up_ns_base * ones(size(threshold)), '--', 'LineWidth', 2.0);
-legend('Detekcja podczas skupienia','Detekcja podczas braku skupienia', 'Location','best');
+% plot(threshold, up_s_base * ones(size(threshold)), '-', 'LineWidth', 2.0);
+% set(gca,'ColorOrderIndex',3)
+% plot(threshold, up_ns_base * ones(size(threshold)), '--', 'LineWidth', 2.0);
+legend('Detekcja podczas skupienia','Detekcja podczas braku skupienia', 'Location','west');
 
 up1500 = figure;
 hold on
@@ -87,10 +87,10 @@ xlim([0.1 1]); ylim([0 100])
 xlabel('Threshold'); ylabel('Stosunek detekcji do iloœci prób [%]')
 plot(threshold, up_s_1500, '-o');
 plot(threshold, up_ns_1500, '--s');
-plot(threshold, up_s_base * ones(size(threshold)), '-', 'LineWidth', 2.0);
-set(gca,'ColorOrderIndex',3)
-plot(threshold, up_ns_base * ones(size(threshold)), '--', 'LineWidth', 2.0);
-legend('Detekcja podczas skupienia','Detekcja podczas braku skupienia', 'Location','best');
+% plot(threshold, up_s_base * ones(size(threshold)), '-', 'LineWidth', 2.0);
+% set(gca,'ColorOrderIndex',3)
+% plot(threshold, up_ns_base * ones(size(threshold)), '--', 'LineWidth', 2.0);
+legend('Detekcja podczas skupienia','Detekcja podczas braku skupienia', 'Location','northeast');
 
 up2500 = figure;
 hold on
@@ -98,10 +98,10 @@ xlim([0.1 1]); ylim([0 100])
 xlabel('Threshold'); ylabel('Stosunek detekcji do iloœci prób [%]')
 plot(threshold, up_s_2500, '-o');
 plot(threshold, up_ns_2500, '--s');
-plot(threshold, up_s_base * ones(size(threshold)), '-', 'LineWidth', 2.0);
-set(gca,'ColorOrderIndex',3)
-plot(threshold, up_ns_base * ones(size(threshold)), '--', 'LineWidth', 2.0);
-legend('Detekcja podczas skupienia','Detekcja podczas braku skupienia', 'Location','best');
+% plot(threshold, up_s_base * ones(size(threshold)), '-', 'LineWidth', 2.0);
+% set(gca,'ColorOrderIndex',3)
+% plot(threshold, up_ns_base * ones(size(threshold)), '--', 'LineWidth', 2.0);
+legend('Detekcja podczas skupienia','Detekcja podczas braku skupienia', 'Location','northeast');
 
 set(up500, 'Units', 'Inches');
 pos = get(up500, 'Position');
